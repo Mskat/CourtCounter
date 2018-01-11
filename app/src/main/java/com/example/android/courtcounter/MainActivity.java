156e3ed7bbbbc8f323a6e2a347e948b4f67243e6
mskat <zur.katarzyna@gmail.com>
2018-01-11 18:40:30 +0100

    Added Team B's Methods and reset button

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -9,36 +9,66 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+    }
+    public void resetScore(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
-     * Increase the score by 3 points
+     * Increase the score by 3 points for Team A
      */
-    public void addThreePoints(View view){
+    public void addThreePointsForA(View view){
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Increase the score by 2 points
+     * Increase the score by 2 points  for Team A
      */
-    public void addTwoPoints(View view){
+    public void addTwoPointsForA(View view){
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Increase the score by 1 point
+     * Increase the score by 1 point for Team A
      */
-    public void addOnePoint(View view){
+    public void addOnePointForA(View view){
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score by 3 points for Team B
+     */
+    public void addThreePointsForB(View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score by 2 points  for Team B
+     */
+    public void addTwoPointsForB(View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score by 1 point for Team B
+     */
+    public void addOnePointForB(View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -49,4 +79,11 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
 }
